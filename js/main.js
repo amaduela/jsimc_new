0b78a70adf1922f679ebb05e8ddf32c2a5160894
amaduela <anselmomaduela@gmail.com>
2018-06-22 09:26:29 +0200

    first time launch

---
+++ js/main.js
@@ -0,0 +1,29 @@
+var pacientes = document.querySelectorAll('.paciente');
+
+for (var i = 0; i < pacientes.length; i++) {
+	var paciente = pacientes[i];
+
+	var nome = paciente.querySelector('.inf-nome');
+	var _nome = nome.textContent;
+
+	var peso = paciente.querySelector('.inf-peso');
+	var _peso = peso.textContent;
+
+	var altura = paciente.querySelector('.inf-altura');
+	var _altura = altura.textContent;
+
+	var gordura = paciente.querySelector('.inf-gordura');
+	var _gordura = gordura.textContent;
+
+	var campoIMC = paciente.querySelector('.inf-imc');
+
+	if (_peso <= 0 || _peso >= 1000) {
+		campoIMC.textContent = "Peso inválido!";
+		paciente.style.backgroundColor = "red";
+	}else if (_altura <= 0 || _altura >= 3.0) {
+		campoIMC.textContent = "Altura inválida!";
+	}else {
+		var imc = _peso / (_altura * _altura);
+		campoIMC.textContent = imc.toFixed(2);
+	}
+}
