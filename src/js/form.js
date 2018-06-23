1a67cd0c45dfa4b1c9061d039f092f68d629f15a
amaduela <anselmomaduela@gmail.com>
2018-06-23 17:16:39 +0200

    using the reset method of the form object

--- src/js/form.js
+++ src/js/form.js
@@ -10,6 +10,8 @@ botaoAdicionar.addEventListener('click', function(event) {
 	
 	var tabela = document.querySelector("#tabela-pacientes");
 	tabela.appendChild(pacienteTr);
+
+	form.reset();
 });
 
 function getData (form) {
