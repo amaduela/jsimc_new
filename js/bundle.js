00601475a768dd5f0c2740a156777759a4e94b16
amaduela <anselmomaduela@gmail.com>
2018-06-23 11:22:52 +0200

    compiled and combined js

--- js/bundle.js
+++ js/bundle.js
@@ -1,3 +1,40 @@
+var pacientes = document.querySelectorAll('.paciente');
+
+for (var i = 0; i < pacientes.length; i++) {
+	var paciente = pacientes[i];
+
+	var nome = paciente.querySelector('.inf-nome');
+	var _nome = nome.textContent;
+
+	var peso = paciente.querySelector('.inf-peso');
+	var _peso = peso.textContent;
+
+	var altura = paciente.querySelector('.inf-altura');
+	var _altura = altura.textContent;
+
+	var gordura = paciente.querySelector('.inf-gordura');
+	var _gordura = gordura.textContent;
+
+	var campoIMC = paciente.querySelector('.inf-imc');
+
+	if (_peso <= 0 || _peso >= 600) {
+		campoIMC.textContent = "Peso inválido!";
+		paciente.classList.add('paciente-invalido');
+	}else if (_altura <= 0 || _altura >= 3.0) {
+		campoIMC.textContent = "Altura inválida!";
+		paciente.classList.add('paciente-invalido');
+	}else {
+		var imc = calculaImc(_peso, _altura);
+		campoIMC.textContent = imc;
+	}
+}
+
+function calculaImc(peso, altura) {
+	var imc = 0;
+	imc = peso / (altura * altura);
+
+	return imc.toFixed(2);
+}
 var botaoAdicionar = document.querySelector("#adicionar-paciente");
 botaoAdicionar.addEventListener('click', function(event) {
 	event.preventDefault();
@@ -31,33 +68,3 @@ botaoAdicionar.addEventListener('click', function(event) {
 	var tabela = document.querySelector("#tabela-pacientes");
 	tabela.appendChild(pacienteTr);
 });
-var pacientes = document.querySelectorAll('.paciente');
-
-for (var i = 0; i < pacientes.length; i++) {
-	var paciente = pacientes[i];
-
-	var nome = paciente.querySelector('.inf-nome');
-	var _nome = nome.textContent;
-
-	var peso = paciente.querySelector('.inf-peso');
-	var _peso = peso.textContent;
-
-	var altura = paciente.querySelector('.inf-altura');
-	var _altura = altura.textContent;
-
-	var gordura = paciente.querySelector('.inf-gordura');
-	var _gordura = gordura.textContent;
-
-	var campoIMC = paciente.querySelector('.inf-imc');
-
-	if (_peso <= 0 || _peso >= 600) {
-		campoIMC.textContent = "Peso inválido!";
-		paciente.classList.add('paciente-invalido');
-	}else if (_altura <= 0 || _altura >= 3.0) {
-		campoIMC.textContent = "Altura inválida!";
-		paciente.classList.add('paciente-invalido');
-	}else {
-		var imc = _peso / (_altura * _altura);
-		campoIMC.textContent = imc.toFixed(2);
-	}
-}
