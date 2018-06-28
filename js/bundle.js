8a763d5cf9586722b40dd57cda3d6b4dba43d412
amaduela <anselmomaduela@gmail.com>
2018-06-28 08:30:52 +0200

    correcting the url

--- js/bundle.js
+++ js/bundle.js
@@ -171,7 +171,7 @@ addButton.addEventListener("click", function () {
 
 	var xhr = new XMLHttpRequest();
 
-	xhr.open("GET", "https://api-pacientes.herokuapp.com/psacientes");
+	xhr.open("GET", "https://api-pacientes.herokuapp.com/pacientes");
 
 	xhr.addEventListener("load", function () {
 		var erros = [];
