ae436ca5bc7aecd1626c3e80a1d417517694ed50
amaduela <anselmomaduela@gmail.com>
2018-06-28 08:28:10 +0200

    validating ajax request status

--- src/js/buscar.js
+++ src/js/buscar.js
@@ -5,15 +5,25 @@ addButton.addEventListener("click", function () {
 
 	var xhr = new XMLHttpRequest();
 
-	xhr.open("GET", "https://api-pacientes.herokuapp.com/pacientes");
+	xhr.open("GET", "https://api-pacientes.herokuapp.com/psacientes");
 
 	xhr.addEventListener("load", function () {
-		var result = xhr.responseText;
-		var ginastas = JSON.parse(result);
+		var erros = [];
+		var errorAjax = document.querySelector("#error-ajax");
+		if (xhr.status == 200) {
+			errorAjax.classList.add('invisible');
+			var result = xhr.responseText;
+			var ginastas = JSON.parse(result);
 
-		ginastas.forEach(function (element) {
-			addOnTable(element);
-		});
+			ginastas.forEach(function (element) {
+				addOnTable(element);
+			});
+		}else {
+			console.log(xhr.status);
+			console.log(xhr.responseText);
+			errorAjax.classList.remove('invisible');
+			errorAjax.textContent = xhr.responseText;
+		}
 	});
 	xhr.send();
 });
