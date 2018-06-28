a8c6e2d50fe82a777cc9d790f75861756029b686
amaduela <anselmomaduela@gmail.com>
2018-06-28 07:09:28 +0200

    bug duplicated new keywword removed

--- src/js/buscar.js
+++ src/js/buscar.js
@@ -3,13 +3,13 @@ var addButton = document.querySelector("#buscar");
 addButton.addEventListener("click", function () {
 	console.log('loading ...');
 
-	var xhr = new new XMLHttpRequest();
+	var xhr = new XMLHttpRequest();
 
 	xhr.open("GET", "https://api-pacientes.herokuapp.com/pacientes");
 
 	xhr.addEventListener("load", function () {
 		var result = xhr.responseText;
-		console.log(result);
+		var ginastas = JSON.parse(result);
 	});
 	xhr.send();
 });
