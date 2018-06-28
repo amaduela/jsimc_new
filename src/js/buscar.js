b7d16faf7189ec5fcf72ff884f71abd9f2916008
amaduela <anselmomaduela@gmail.com>
2018-06-28 07:05:31 +0200

    script that request data from an external api

---
+++ src/js/buscar.js
@@ -0,0 +1,15 @@
+var addButton = document.querySelector("#buscar");
+
+addButton.addEventListener("click", function () {
+	console.log('loading ...');
+
+	var xhr = new new XMLHttpRequest();
+
+	// xhr.open("GET", "https://api-pacientes.herokuapp.com/pacientes");
+
+	// xhr.addEventListener("load", function () {
+	// 	var result = xhr.responseText;
+	// 	console.log(result);
+	// });
+	// xhr.send();
+});
