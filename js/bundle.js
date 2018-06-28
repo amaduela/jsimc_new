4ba2445640aea8d42a197438818d0a5ce6cc62e1
amaduela <anselmomaduela@gmail.com>
2018-06-28 06:14:52 +0200

    eventLinstener input compiled

--- js/bundle.js
+++ js/bundle.js
@@ -137,3 +137,21 @@ tabela.addEventListener("dbclick", function (event) {
 		event.target.parentNode.remove();
 	}, 500);
 });
+var filter = document.querySelector("#filtrar-tabela");
+
+filter.addEventListener("input", function () {
+	console.log(this.value);
+	var ginastas = document.querySelectorAll(".ginasta");
+
+	for(var i = 0; i < ginastas.length; i++){
+		var ginasta = ginastas[i];
+		tdNome = ginasta.querySelector(".inf-nome");
+		var nome = tdNome.textContent;
+
+		if (nome != this.value) {
+			ginasta.classList.add('invisible');
+		}else {
+			ginasta.classList.remove('invisible');
+		}
+	}
+});
